5f5bdc638b9d8fa2a9484dc2fd33612946b92cb5
louiscruz <louisstephancruz@me.com>
2017-08-25 16:52:42 -0700

    Add initial jest

---
+++ __tests__/ReactVoronoi-test.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+import {
+  describe,
+  test,
+  expect,
+} from 'jest';
+
+// import ReactVoronoi from '../ReactVoronoi';
+
+describe('ReactVoronoi', () => {
+  test('exists', () => {
+    expect(true).toBeTruthy();
+  });
+});
